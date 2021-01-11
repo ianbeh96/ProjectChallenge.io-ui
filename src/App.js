f832ab27dbd3315985ee1c263507fa9666beea0a
Ian Beh <ianbeh@Ians-MacBook-Pro.local>
2021-01-11 00:53:21 -0600

    Initial files commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HomePage from './views/homePage';
+import LoginPage from './views/loginPage';
+import RegisterPage from './views/registerPage';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/homePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class homePage extends React.Component {
+    render() {
+        return (
+          <div>
+            <p>Hello World!</p>
+          </div>
+        )
+      }
+}
+
+export default homePage;
